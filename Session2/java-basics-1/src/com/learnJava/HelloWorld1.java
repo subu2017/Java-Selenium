7b3428162b2e8822975150f775c7351f70dd4d3d
Dilip.Sundarraj <Dilip.Sundarraj@target.com>
2017-09-25 09:46:54 -0500

    added the different sessions

---
+++ Session2/java-basics-1/src/com/learnJava/HelloWorld1.java
@@ -0,0 +1,19 @@
+package com.learnJava;
+
+/**
+ * Created by z001qgd on 9/24/17.
+ */
+public class HelloWorld1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Inside Hello World1 ");
+
+        HelloWorld helloWorld = new HelloWorld();
+
+        helloWorld.main1();
+       // helloWorld.main2();
+
+
+    }
+}
