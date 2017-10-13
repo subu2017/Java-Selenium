e37032c6143b61ef60468e7885762debc1c83ccc
dilipthelip <dilip.thelip@gmail.com>
2017-10-13 08:29:38 -0500

    addd the login page code

--- code/carrierincentives/src/main/java/com/learnselenium/carrierincentives/login/CarrierIncentiveLogin.java
+++ code/carrierincentives/src/main/java/com/learnselenium/carrierincentives/login/CarrierIncentiveLogin.java
@@ -1,5 +1,6 @@
 package com.learnselenium.carrierincentives.login;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -18,7 +19,21 @@ public class CarrierIncentiveLogin {
 		    webDriver.get(URL);
 		    Thread.sleep(4000);
 		    
+		    webDriver.findElement(By.xpath(".//*[@id='app_20']")).click(); //Clicking the login page
 		    
+		    webDriver.findElement(By.xpath(".//*[@id='UserName']")).sendKeys("dilip"); //Sending the user name
+		    
+		    webDriver.findElement(By.xpath(".//*[@id='PASSWORD']")).sendKeys("dilip");//Sending the password
+		    
+		    Thread.sleep(6000);
+
+		    webDriver.findElement(By.id("loginButton")).click();//Clicking the login button
+
+		   
+		    Thread.sleep(4000);
+		    
+		    
+
 	    }catch(Exception e){
 	    	e.printStackTrace();
 	    	System.out.println("` Exception is : " + e);
